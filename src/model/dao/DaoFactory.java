38f2980ad4af9f517dbda0fba9bb73a893752870
AndreSylva <andre.lucyo2@gmail.com>
2020-07-12 15:08:02 -0300

    secao 21 - Implemetado o CRUD do DepartmentDao e Program2 para Test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //Fabrica de DAO
@@ -14,6 +15,14 @@ public class DaoFactory {
 	 */
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	/***
+	 * Fabria de DAO com o uso de JDBC
+	 * @return
+	 */
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 	
 }

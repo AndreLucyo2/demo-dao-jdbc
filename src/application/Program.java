ad225ee4cb97553e4520c0314da89c5464c147a5
AndreSylva <andre.lucyo2@gmail.com>
2020-07-12 11:07:19 -0300

    Implementando findById - SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,19 +12,13 @@ public class Program
 
 	public static void main(String[] args)
 	{
-		Department obj = new Department(1, "Books");
 
-		Seller newSeller = new Seller(21, "Bob", "bob@gmail.com", new Date(0), 4000.0, obj);
-
-		// Chama o metodo estatico:
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-
-		System.out.println("\n===  DEPARTAMENTO  ==============================================================");
-
-		System.out.println(obj);
-
+		SellerDao sellerDao  = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println("\n===  VENDEDOR  ==================================================================");
 
-		System.out.println(newSeller);
+		System.out.println(seller);
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,7 +1,13 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -9,32 +15,89 @@ import model.entities.Seller;
 public class SellerDaoJDBC implements SellerDao
 {
 
+	private Connection conn;
+	//Força a injeção de dependencia:
+	//Ao instanciar ja vem com a conexão definida:
+	public SellerDaoJDBC(Connection conn)
+	{
+		this.conn = conn;
+	}
+	
+	
+
 	@Override
 	public void insert(Seller obj)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try
+		{
+			//Inicia o prepareStatement:
+			st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
+
+			//Passa o id para consulta
+			st.setInt(1, id);
+			//Recee o resultado:
+			rs = st.executeQuery();
+			
+			//testa se veio algum resultado: Tranforma os dados da tabela em objetos
+			if (rs.next())
+			{
+				//DEPARTAMENTO
+				Department dep = new Department();
+				//Nome das colunas da consulta
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));					
+				
+				//VENDEDOR:
+				Seller obj = new Seller();
+				//Nome das colunas da consulta
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setName(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				
+				return obj;
+			}
+			
+			//se nao vem resultado retorna null
+			return null;
+		}
+		catch (SQLException e)//Captura e exceção de SQL
+		{
+			throw new DbException(e.getMessage());
+		}
+		finally
+		{
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
@@ -51,5 +114,4 @@ public class SellerDaoJDBC implements SellerDao
 		return null;
 	}
 
-	
 }

7ae21be5f36e15be7b6fd27c9fd8621103bc718e
AndreSylva <andre.lucyo2@gmail.com>
2020-07-12 00:33:57 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,27 @@
 package application;
 
+import java.sql.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program
 {
 
 	public static void main(String[] args)
 	{
-		Department obj = new Department(1, "Books");
+		Department obj = new Department(1, "Books");		
+		
+		Seller newSeller = new Seller(21, "Bob", "bob@gmail.com", new Date(0), 4000.0, obj);
+		
+		
+		
+		System.out.println("\n===  DEPARTAMENTO  ==============================================================");
+		
 		System.out.println(obj);
 		
+		System.out.println("\n===  VENDEDOR  ==================================================================");
+		
+		System.out.println(newSeller);
 	}
 }
